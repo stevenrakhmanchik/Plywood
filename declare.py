83353358c438bf026fbed500b8e505580a996e44
Steven Rakhmanchik <48323187+stevenrakhmanchik@users.noreply.github.com>
2022-05-26 13:16:54 -0400

    Add files via upload

---
+++ declare.py
@@ -0,0 +1,60 @@
+from py_expression_eval import Parser
+import errorhandler as eh
+import g
+
+def declare(line):
+    parser = Parser()
+    acceptable = ('integer','float','numerical','string','nulltype')
+    banned = ()
+    line_lex = line.split('->')
+    loc = eh.getLine(line) + 1
+    expression = line_lex[0]
+    varName = line_lex[1].strip()
+    if len(line_lex) > 2:
+        eh.multiDeclare(loc)
+    if varName.isalpha() == False:
+        eh.invalidVarName(loc)
+    if varName in g.varNames and not(varName in banned):
+        a = g.varNames.index(varName)
+        varNameOld = varName
+        oldValue = g.varValues[a]
+        varType = g.varTypes[a]
+        if not(varType in acceptable):
+            eh.varExistNotNumerical(loc)
+        else:
+            g.varTypes.pop(a)
+            g.varValues.pop(a)
+            g.varNames.pop(a)
+    g.varTypes.append('nulltype')
+    g.varNames.append(varName)
+    g.varValues.append(0.0)
+    varsFound = parser.parse(expression).variables()
+    varDict = {}
+    flagString = False
+    for x in varsFound:
+        try:
+            a = g.varNames.index(x)
+        except:
+            eh.varDoesntExist(loc)
+        if not(g.varTypes[a] in acceptable):
+            eh.varInWrongContext(loc)
+        if g.varTypes[a] == 'string': flagString = True
+        if x == varName:
+            varDict[x] = oldValue
+            continue
+        varDict[x] = g.varValues[a]
+    try:
+        newValue = parser.parse(expression).evaluate(varDict)
+    except TypeError:
+        if flagString == True:
+            varDictTemp = {}
+            for var in varDict:
+                varDictTemp[var] = str(varDict[var])
+            varDict = varDictTemp
+            newValue = parser.parse(expression).evaluate(varDict)
+    if type(newValue) == type(0.1): (g.varTypes[len(g.varTypes) - 1]) = 'float'
+    if type(newValue) == type(1): (g.varTypes[len(g.varTypes) - 1]) = 'integer'
+    if type(newValue) == type(""): (g.varTypes[len(g.varTypes) - 1]) = 'string'
+    if not(g.varTypes[len(g.varTypes) - 1]) in acceptable:
+        eh.declarationNonNumerical(loc)
+    g.varValues[len(g.varValues) - 1] = newValue
---
+++ output.py
@@ -0,0 +1,65 @@
+import g
+import errorhandler as eh
+from py_expression_eval import Parser
+
+# if var.isalpha():
+#     try:
+#         a = g.varNames.index(var)
+#     except:
+#         eh.varDoesntExist(loc)
+#     if g.varTypes[a] != "float":
+#         eh.wrongTypeInFormat(loc)
+#     print(g.varValues[a], end = '')
+# elif var.isdecimal():
+#     if '.' in var:
+#         eh.wrongTypeInFormat(loc)
+#     print(var, end = '')
+
+def escape_split(str_to_escape, delimiter='$', escape='\\'):
+    if len(delimiter) > 1 or len(escape) > 1:
+        raise ValueError("Either delimiter or escape must be an one char value")
+    token = ''
+    escaped = False
+    for c in str_to_escape:
+        if c == escape:
+            if escaped:
+                token += escape
+                escaped = False
+            else:
+                escaped = True
+            continue
+        if c == delimiter:
+            if not escaped:
+                yield token
+                token = ''
+            else:
+                token += c
+                escaped = False
+        else:
+            if escaped:
+                token += escape
+                escaped = False
+            token += c
+    yield token
+
+
+def out(line):
+    loc = eh.getLine("out" + line) + 1
+    varMatch = line.strip()
+    varMatch = list(escape_split(varMatch, '$'))
+    parser = Parser()
+    for x in range(0,len(varMatch)):
+        var = varMatch[x]
+        try:
+            varsFound = parser.parse(var).variables()
+        except:
+            eh.varDoesntExist(loc)
+        varDict = {}
+        for x in varsFound:
+            try:
+                a = g.varNames.index(x)
+            except:
+                eh.varDoesntExist(loc)
+            varDict[x] = g.varValues[a]
+        newValue = parser.parse(var).evaluate(varDict)
+        print(newValue, end = '')
